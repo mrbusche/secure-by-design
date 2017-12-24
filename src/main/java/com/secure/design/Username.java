ee79f6d634564e8e33434a66dd36a7574c1e04a5
Matt Busche <mrbusche@gmail.com>
2017-12-23 22:15:26 -0600

    More validation around username
    Special character and length validation

--- src/main/java/com/secure/design/Username.java
+++ src/main/java/com/secure/design/Username.java
@@ -1,14 +1,19 @@
 package com.secure.design;
 
+import static org.apache.commons.lang3.Validate.inclusiveBetween;
 import static org.apache.commons.lang3.Validate.isTrue;
 import static org.apache.commons.lang3.Validate.notNull;
 
 public class Username {
     private final String username;
+    private final int minLength = 3;
+    private final int maxLength = 20;
+    private final String regEx = "^[a-zA-Z0-9._-]{3,}$";
 
     Username(final String username) {
         notNull(username, "Username cannot be null");
-        isTrue(username.length() < 20, "Username can only be 20 characters");
+        inclusiveBetween(minLength, maxLength, username.length(), "username must be between 3 and 20 characters");
+        isTrue(username.matches(regEx), "username can only contain letters, numbers, -, _ and .");
         this.username = username;
     }
 }
--- src/test/java/com/secure/design/UsernameTest.groovy
+++ src/test/java/com/secure/design/UsernameTest.groovy
@@ -1,6 +1,7 @@
 package com.secure.design
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class UsernameTest extends Specification {
 
@@ -13,12 +14,91 @@ class UsernameTest extends Specification {
         username == null
     }
 
-    def "check username less than 20 characters"() {
+    @Unroll
+    def "check username between 3 and 20 characters throws Exception #input"() {
         when:
-        Username username = new Username('abcdefghijklmnopqrstuvwxyz')
+        Username username = new Username(input)
         then:
-        def e = thrown(IllegalArgumentException)
-        e.message == 'Username can only be 20 characters'
+        IllegalArgumentException e = thrown(IllegalArgumentException)
+        e.message == errorMessage
         username == null
+        where:
+        input                        | errorMessage
+        'a'                          | 'username must be between 3 and 20 characters'
+        'ab'                         | 'username must be between 3 and 20 characters'
+        'abcdefghijklmnopqrstu'      | 'username must be between 3 and 20 characters'
+        'abcdefghijklmnopqrstuvwxyz' | 'username must be between 3 and 20 characters'
     }
+
+    @Unroll
+    def "check username between 3 and 20 characters #input"() {
+        when:
+        Username username = new Username(input)
+        then:
+        username.username == input
+        where:
+        input                  | blank
+        'abc'                  | ''
+        'abcd'                 | ''
+        'abcde'                | ''
+        'abcdef'               | ''
+        'abcdefg'              | ''
+        'abcdefgh'             | ''
+        'abcdefghi'            | ''
+        'abcdefghij'           | ''
+        'abcdefghijk'          | ''
+        'abcdefghijkl'         | ''
+        'abcdefghijklm'        | ''
+        'abcdefghijklmn'       | ''
+        'abcdefghijklmno'      | ''
+        'abcdefghijklmnop'     | ''
+        'abcdefghijklmnopq'    | ''
+        'abcdefghijklmnopqr'   | ''
+        'abcdefghijklmnopqrs'  | ''
+        'abcdefghijklmnopqrst' | ''
+    }
+
+    @Unroll
+    def "check username format valid #input"() {
+        when:
+        Username username = new Username(input)
+        then:
+        username.username == input
+        where:
+        input         | blank
+        'abc'         | ''
+        'abc_'        | ''
+        'abc_aa'      | ''
+        'abc123'      | ''
+        'abc123_'     | ''
+        'abc_123'     | ''
+        'abc__def'    | ''
+        'abc_d-e.f'   | ''
+        'abc1de.f'    | ''
+        'abc_d-22ef'  | ''
+        'abc_def.ghi' | ''
+    }
+
+    @Unroll
+    def "check username alphanumeric or _ throws Exception #input"() {
+        when:
+        Username username = new Username(input)
+        then:
+        IllegalArgumentException e = thrown(IllegalArgumentException)
+        e.message == errorMessage
+        username == null
+        where:
+        input        | errorMessage
+        'ab@'        | 'username can only contain letters, numbers, -, _ and .'
+        'ab#'        | 'username can only contain letters, numbers, -, _ and .'
+        'a1$'        | 'username can only contain letters, numbers, -, _ and .'
+        'a2%'        | 'username can only contain letters, numbers, -, _ and .'
+        'a4('        | 'username can only contain letters, numbers, -, _ and .'
+        'a5)'        | 'username can only contain letters, numbers, -, _ and .'
+        'a6!'        | 'username can only contain letters, numbers, -, _ and .'
+        'a123#'      | 'username can only contain letters, numbers, -, _ and .'
+        'akljdad&'   | 'username can only contain letters, numbers, -, _ and .'
+        'a7^'        | 'username can only contain letters, numbers, -, _ and .'
+    }
+
 }
