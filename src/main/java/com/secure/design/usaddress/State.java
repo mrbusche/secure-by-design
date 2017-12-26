790e462ed361bbe0ffc646f3bb9faa1c7d0405ca
Matt Busche <mrbusche@gmail.com>
2017-12-25 22:49:42 -0600

    add valid state list and tests

--- src/main/java/com/secure/design/usaddress/State.java
+++ src/main/java/com/secure/design/usaddress/State.java
@@ -1,12 +1,21 @@
 package com.secure.design.usaddress;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.apache.commons.lang3.Validate.isTrue;
 import static org.apache.commons.lang3.Validate.notNull;
 
 public class State {
     private final String state;
+    private static final Set<String> stateList = new HashSet<>(Arrays.asList(
+            "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"));
 
     State(final String state) {
         notNull(state, "state cannot be null");
+        isTrue(state.length() == 2, "state must be a valid state abbreviation");
+        isTrue(stateList.contains(state), "state must be a valid state abbreviation");
         this.state = state;
     }
 }
--- src/test/java/com/secure/design/usaddress/StateTest.groovy
+++ src/test/java/com/secure/design/usaddress/StateTest.groovy
@@ -1,6 +1,7 @@
 package com.secure.design.usaddress
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class StateTest extends Specification {
 
@@ -11,5 +12,86 @@ class StateTest extends Specification {
         def e = thrown(NullPointerException)
         e.message == 'state cannot be null'
         state == null
+    }
+
+    @Unroll
+    def "check state exactly 2 alpha characters #input"() {
+        when:
+        State state = new State(input)
+        then:
+        IllegalArgumentException e = thrown(IllegalArgumentException)
+        e.message == errorMessage
+        state == null
+        where:
+        input       | errorMessage
+        '12345678a' | 'state must be a valid state abbreviation'
+        '12345678b' | 'state must be a valid state abbreviation'
+        'abcdefgh0' | 'state must be a valid state abbreviation'
+        'abcdefghi' | 'state must be a valid state abbreviation'
+        'aa'        | 'state must be a valid state abbreviation'
+        'AA'        | 'state must be a valid state abbreviation'
+        'Bc'        | 'state must be a valid state abbreviation'
+        'ar'        | 'state must be a valid state abbreviation'
+        'wy'        | 'state must be a valid state abbreviation'
+    }
+
+    @Unroll
+    def "check state is valid abbreviation #input"() {
+        when:
+        State state = new State(input)
+        then:
+        state.state == input
+        where:
+        input | blank
+        'AL'  | ''
+        'AK'  | ''
+        'AZ'  | ''
+        'AR'  | ''
+        'CA'  | ''
+        'CO'  | ''
+        'CT'  | ''
+        'DE'  | ''
+        'FL'  | ''
+        'GA'  | ''
+        'HI'  | ''
+        'ID'  | ''
+        'IL'  | ''
+        'IN'  | ''
+        'IA'  | ''
+        'KS'  | ''
+        'KY'  | ''
+        'LA'  | ''
+        'ME'  | ''
+        'MD'  | ''
+        'MA'  | ''
+        'MI'  | ''
+        'MN'  | ''
+        'MS'  | ''
+        'MO'  | ''
+        'MT'  | ''
+        'NE'  | ''
+        'NV'  | ''
+        'NH'  | ''
+        'NJ'  | ''
+        'NM'  | ''
+        'NY'  | ''
+        'NC'  | ''
+        'ND'  | ''
+        'OH'  | ''
+        'OK'  | ''
+        'OR'  | ''
+        'PA'  | ''
+        'RI'  | ''
+        'SC'  | ''
+        'SD'  | ''
+        'TN'  | ''
+        'TX'  | ''
+        'UT'  | ''
+        'VT'  | ''
+        'VA'  | ''
+        'WA'  | ''
+        'WV'  | ''
+        'WI'  | ''
+        'WY'  | ''
     }
 }
