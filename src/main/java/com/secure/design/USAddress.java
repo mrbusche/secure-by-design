f261892df8419805afa4ca509bf4a533b4509401
Matt Busche <mrbusche@gmail.com>
2017-12-25 22:19:36 -0600

    clean up some declarations

--- src/main/java/com/secure/design/USAddress.java
+++ src/main/java/com/secure/design/USAddress.java
@@ -3,12 +3,12 @@ package com.secure.design;
 import com.secure.design.usaddress.*;
 
 public class USAddress {
-    private StreetAddress1 streetAddress1;
-    private StreetAddress2 streetAddress2;
-    private City city;
-    private State state;
-    private Zip5 zip5;
-    private Zip4 zip4;
+    private final StreetAddress1 streetAddress1;
+    private final StreetAddress2 streetAddress2;
+    private final City city;
+    private final State state;
+    private final Zip5 zip5;
+    private final Zip4 zip4;
 
     USAddress(final StreetAddress1 streetAddress1, final StreetAddress2 streetAddress2, final City city, final State state, Zip5 zip5, Zip4 zip4) {
         this.streetAddress1 = streetAddress1;
