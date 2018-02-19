2bee1a63635e509fa3d4ce3365dadb694d5ed1c4
Matt Busche <mrbusche@gmail.com>
2018-02-18 21:20:49 -0600

    rename controller
    remove default method
    add save address method

--- src/main/java/com/secure/demo/controller/AddressController.java
+++ src/main/java/com/secure/demo/controller/AddressController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class DemoController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+public class AddressController {
 
     @RequestMapping("/address")
     public ModelAndView address() {
+        return new ModelAndView("address");
+    }
+
+    @RequestMapping("/saveAddress")
+    public ModelAndView saveAddress() {
         String message = "Hello World";
         return new ModelAndView("address", "message", message);
     }
